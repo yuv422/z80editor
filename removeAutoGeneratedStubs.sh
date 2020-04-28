2d9d27707295790cbcc7624e1c52860e08414e5e
Eric Fry <yuv422@users.noreply.github.com>
2020-04-29 08:18:02 +1000

    Added repository project. Added script to remove autogenerated stub classes. (I wish it wouldn't generate these everytime.)

---
+++ removeAutoGeneratedStubs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rm org.efry.z80editor.ide/src/org/efry/z80editor/ide/Z80IdeModule.xtend
+rm org.efry.z80editor.ide/src/org/efry/z80editor/ide/Z80IdeSetup.xtend
+rm org.efry.z80editor.ui/src/org/efry/z80editor/ui/Z80UiModule.xtend
+rm org.efry.z80editor.ui/src/org/efry/z80editor/ui/contentassist/Z80ProposalProvider.xtend
+rm org.efry.z80editor.ui/src/org/efry/z80editor/ui/labeling/Z80DescriptionLabelProvider.xtend
+rm org.efry.z80editor.ui/src/org/efry/z80editor/ui/quickfix/Z80QuickfixProvider.xtend
+rm org.efry.z80editor/src/org/efry/z80editor/Z80RuntimeModule.xtend
+rm org.efry.z80editor/src/org/efry/z80editor/Z80StandaloneSetup.xtend
